2ef3a37ac231ce806d772ce1013076c037bf978f
RicardoDB8 <bertarini@gmail.com>
2025-02-11 20:25:42 -0300

    Sub Menus

--- src/script.js
+++ src/script.js
@@ -22,3 +22,45 @@ function reloadPage(event) {
     event.preventDefault();
     window.location.reload();
 }
+
+
+// Sub menus
+document.addEventListener("DOMContentLoaded", function () {
+    var submenus = document.querySelectorAll(".submenu");
+
+    // Ocultar todos los submenús al cargar la página
+    submenus.forEach(function (submenu) {
+        submenu.style.display = "none";
+    });
+
+    // Agregar eventos a los enlaces dentro de los submenús para que se cierren al hacer clic
+    var submenuLinks = document.querySelectorAll(".submenu a");
+    submenuLinks.forEach(function (link) {
+        link.addEventListener("click", function () {
+            var submenu = this.closest(".submenu");
+            if (submenu) {
+                submenu.style.display = "none";
+            }
+        });
+    });
+});
+
+function toggleSubmenu(event, submenuId) {
+    event.preventDefault(); // Evita la navegación predeterminada
+
+    var submenus = document.querySelectorAll(".submenu");
+
+    // Cierra todos los submenús
+    submenus.forEach(function (submenu) {
+        if (submenu.id !== submenuId) {
+            submenu.style.display = "none";
+        }
+    });
+
+    var submenu = document.getElementById(submenuId);
+
+    // Alterna el submenú seleccionado
+    submenu.style.display = submenu.style.display === "block" ? "none" : "block";
+}
+
+
